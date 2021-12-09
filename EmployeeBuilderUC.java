9937c9807ddfe15a56942b1998356c334d76733f
SandeshMarathe <marathesandesh1999@gmail.com>
2021-12-09 08:50:33 +0530

    ADDED : Program To check Total Working Hour or Days is Reached for Month

--- EmployeeBuilderUC.java
+++ EmployeeBuilderUC.java
@@ -4,16 +4,20 @@ public class EmployeeBuilderUC
 	public static final int IS_FULL_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
 	public static final int NUM_OF_WORKING_DAYS=20;
+	public static final int MAX_HRS_IN_MONTHS=100;
 
 	public void checkPresentOrAbsent()
 	{
 		int empHrs=0;
-		int empWage=0;
-		int totalEmpWage=0;
-		int empCheck =(int) Math.floor(Math.random() * 10) % 3;
+		int totalEmpHrs=0;
+		int totalWorkingDays=0;
 
-		for (int day=0;day<NUM_OF_WORKING_DAYS;day++)
+		while (totalEmpHrs<=MAX_HRS_IN_MONTHS && totalWorkingDays<NUM_OF_WORKING_DAYS)
 		{
+
+			totalWorkingDays++;
+
+			int empCheck =(int) Math.floor(Math.random() * 10) % 3;
 
 			switch (empCheck)
 			{
@@ -28,12 +32,12 @@ public class EmployeeBuilderUC
 						break;
 			}
 
-			empWage = empHrs*EMP_RATE_PER_HOUR;
-			totalEmpWage +=empWage;
-			System.out.println("Emp Wage :"+empWage);
+			totalEmpHrs += empHrs;
+			System.out.println("Day# :"+totalWorkingDays+"Emp Hrs: "+empHrs);
 		}
-		System.out.println("Total Emp Wage :"+totalEmpWage);
 
+		int totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
+		System.out.println("total Emp Wage : "+totalEmpWage);
 	}
 
 	public static void main(String args[])
