1a44901e9d2e45dcf5190ef09ef65d585e7ad7ef
SandeshMarathe <marathesandesh1999@gmail.com>
2021-12-09 08:23:02 +0530

    ADDED : Program To Check Employee Add Part Time Wage

--- EmployeeBuilderUC.java
+++ EmployeeBuilderUC.java
@@ -1,6 +1,7 @@
 public class EmployeeBuilderUC
 {
-	int IS_FULL_TIME=1;
+	int IS_PART_TIME=1;
+	int IS_FULL_TIME=2;
 	int EMP_RATE_PER_HOUR=20;
 
 	public int checkPresentOrAbsent(int empCheck)
@@ -8,7 +9,11 @@ public class EmployeeBuilderUC
 		int empHrs=0;
 		int empWage=0;
 
-		if (empCheck == IS_FULL_TIME)
+		if (empCheck == IS_PART_TIME)
+		{
+			empHrs=4;
+		}
+		else if (empCheck == IS_FULL_TIME)
 		{
 			empHrs=8;
 		}
@@ -25,7 +30,7 @@ public class EmployeeBuilderUC
 	public static void main(String args[])
 	{
 
-		int empCheck =(int) Math.floor(Math.random() * 10) % 2;
+		int empCheck =(int) Math.floor(Math.random() * 10) % 3;
 
 		EmployeeBuilderUC eb = new EmployeeBuilderUC();
 		System.out.println("Emp Wage : "+eb.checkPresentOrAbsent(empCheck));
