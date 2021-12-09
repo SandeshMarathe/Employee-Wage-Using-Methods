7eaf9f2bdc2df5ea6f5ff76ea8f76446b9df7f24
SandeshMarathe <marathesandesh1999@gmail.com>
2021-12-09 08:40:43 +0530

    ADDED : Program To Check Employee Wage Of Month

--- EmployeeBuilderUC.java
+++ EmployeeBuilderUC.java
@@ -3,36 +3,45 @@ public class EmployeeBuilderUC
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
+	public static final int NUM_OF_WORKING_DAYS=20;
 
-	public int checkPresentOrAbsent(int empCheck)
+	public void checkPresentOrAbsent()
 	{
 		int empHrs=0;
 		int empWage=0;
+		int totalEmpWage=0;
+		int empCheck =(int) Math.floor(Math.random() * 10) % 3;
 
-		switch (empCheck)
+		for (int day=0;day<NUM_OF_WORKING_DAYS;day++)
 		{
-			case IS_PART_TIME:
-					empHrs=4;
-					break;
-			case IS_FULL_TIME:
-					empHrs=8;
-					break;
-			default:
-					empHrs=0;
-		}
-		empWage = empHrs*EMP_RATE_PER_HOUR;
 
-		return empWage;
+			switch (empCheck)
+			{
+				case IS_PART_TIME:
+						empHrs=4;
+						break;
+				case IS_FULL_TIME:
+						empHrs=8;
+						break;
+				default:
+						empHrs=0;
+						break;
+			}
+
+			empWage = empHrs*EMP_RATE_PER_HOUR;
+			totalEmpWage +=empWage;
+			System.out.println("Emp Wage :"+empWage);
+		}
+		System.out.println("Total Emp Wage :"+totalEmpWage);
 
 	}
 
 	public static void main(String args[])
 	{
 
-		int empCheck =(int) Math.floor(Math.random() * 10) % 3;
 
 		EmployeeBuilderUC eb = new EmployeeBuilderUC();
-		System.out.println("Emp Wage : "+eb.checkPresentOrAbsent(empCheck));
+		eb.checkPresentOrAbsent();
 
 	}
 }
