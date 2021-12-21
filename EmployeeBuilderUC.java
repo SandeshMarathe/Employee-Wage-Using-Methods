8fa1ff00c04d3973095e71094250d21d8a5d8581
SandeshMarathe <marathesandesh1999@gmail.com>
2021-12-21 10:11:19 +0530

    ADDED : Java Program Ability to save Toatl Wage for Company

--- EmployeeBuilderUC.java
+++ EmployeeBuilderUC.java
@@ -3,47 +3,72 @@ public class EmployeeBuilderUC
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
 
-	public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+	private static String company;
+	private static int empRatePerHour;
+	private static int numOfWorkingDays;
+	private static int maxHoursPerMonth;
+	private int totalEmpWage;
+
+	public EmployeeBuilderUC (String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+	{
+
+		this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+	}
+
+	public static int computeEmpWage()
 	{
 		//Variables
 		int empHrs=0;
 		int totalEmpHrs=0;
 		int totalWorkingDays=0;
+		int totalEmpWage=0;
 
 		//Computation
-		while (totalEmpHrs<=maxHoursPerMonth && totalWorkingDays<numOfWorkingDays)
+		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
 		{
 
 			totalWorkingDays++;
 
-			int empCheck =(int) Math.floor(Math.random() * 10) % 3;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
 			switch (empCheck)
 			{
 				case IS_PART_TIME:
-						empHrs=4;
+						empHrs = 4;
 						break;
 				case IS_FULL_TIME:
-						empHrs=8;
+						empHrs = 8;
 						break;
 				default:
-						empHrs=0;
+						empHrs = 0;
 						break;
 			}
 
 			totalEmpHrs += empHrs;
-			System.out.println("Day# :"+totalWorkingDays+"Emp Hrs: "+empHrs);
+			System.out.println("Day# :" + totalWorkingDays + "Emp Hrs: " + empHrs);
 		}
 
-		int totalEmpWage=totalEmpHrs*empRatePerHour; 
-		System.out.println("total Emp Wage for Company: "+company+" is :"+totalEmpWage);
-
+		totalEmpWage = totalEmpHrs * empRatePerHour;
 		return totalEmpWage;
+	}
+
+	@Override
+	public String toString()
+	{
+
+		return "Total Emp Wage for Company : " + company + "is: " + totalEmpWage;
 	}
 
 	public static void main(String args[])
 	{
-		computeEmpWage("DMart",20,2,10);
-		computeEmpWage("Reliance",10,4,20);
+		EmployeeBuilderUC dMart = new EmployeeBuilderUC("DMart", 20, 2, 10);
+		EmployeeBuilderUC reliance = new EmployeeBuilderUC("Reliance", 10, 4, 20);
+		dMart.computeEmpWage();
+		System.out.println(dMart);
+		reliance.computeEmpWage();
+		System.out.println(reliance);
 	}
 }
